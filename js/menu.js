304481ba9f6fb17b1df5cd48536c014058a56c9e
Bishnu Upadhyay <bishnuupadhyay012@gmail.com>
2026-05-27 16:57:06 -0400

    inital commit

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+fetch("menu.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const container = document.getElementById("menuContainer");
+
+    data.forEach(section => {
+
+      const card = document.createElement("div");
+      card.className = "menu-card";
+
+      let itemsHTML = "";
+
+      section.items.forEach(item => {
+
+        itemsHTML += `
+          <a href="details.html?id=${item.id}" class="food-link">
+            <div class="item">
+              <span>${item.name}</span>
+              <span>$${item.price}</span>
+            </div>
+          </a>
+        `;
+      });
+
+      card.innerHTML = `
+        <h3>${section.category}</h3>
+        ${itemsHTML}
+      `;
+
+      container.appendChild(card);
+
+    });
+
+  });
